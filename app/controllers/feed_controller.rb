325b85baf17c6dc44943ba5df2215f22ca612937
etika ahuja <etika@9piecesof8.com>
2018-04-04 00:46:29 +0530

    Changes in index action

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -13,10 +13,10 @@ class FeedController < ApplicationController
     parsed_json.map do |json|
       if json['verb'] == "shared"
        url = HTTParty.get("https://feed-challenge-api.herokuapp.com/shares/1").parsed_response['url']
-       @final_array << {url: url,verb: json['verb'],actor: json['actor'], description: 'bla'}
+       @final_array << {url: url,verb: json['verb'],actor: json['actor'], description: "#{json['actor'] } posted #{url}}"}
       else
         content = HTTParty.get("https://feed-challenge-api.herokuapp.com/posts/1").parsed_response['content']
-       @final_array << {content: url,verb: json['verb'],actor: json['actor'], description: 'bla'}
+       @final_array << {content: content,verb: json['verb'],actor: json['actor'], description: "#{json['actor'] } posted #{content}}"}
       end
     end
   end
